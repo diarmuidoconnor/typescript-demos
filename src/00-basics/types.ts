4582f2f68d773c34a9f05477ea2eceeb69808f3b
Diarmuid O Connor <doconnor@wit.ie>
2025-09-18 08:29:20 +0100

    Explain any and unknown types

---
+++ src/00-basics/types.ts
@@ -0,0 +1,12 @@
+
+
+let anyVar : any = "hello world"
+anyVar = [1,2,3]
+
+// -------------------------------
+// let unknownVar: unknown ;
+// //  let result = unknownVar * 2
+
+// if (typeof unknownVar === 'number' ) {
+//   let result = unknownVar* 2 ;
+// }
