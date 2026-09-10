a7e6ecb3923d1504f03b3c75d68a66b5b259e996
Diarmuid O Connor <doconnor@wit.ie>
2026-09-10 08:42:22 +0100

    Claknown typerify un

--- src/00-basics/edgeTypes.ts
+++ src/00-basics/edgeTypes.ts
@@ -4,9 +4,9 @@ let anyVar : any = "hello world"
 anyVar = [1,2,3]
 
 // -------------------------------
-let unknownVar: unknown ;
+let unknownVar: unknown = 10;
+
 //  let result = unknownVar * 2
-// let someValue = unknownVar* 2 ;
 
 if (typeof unknownVar === 'number' ) {
   let result = unknownVar* 2 ;
